5f340bf32eaf821a4d3f5f7dc4316534b3b1683e
AlexWilliams412 <alexw3428@gmail.com>
2024-10-02 11:50:25 -0400

    I updated workbook 1 and 2

---
+++ workbook-1/MathApplication/src/main/java/com/pluralsight/MathApp.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class MathApp {
+    public static void main(String[] args) {
+// Question 1
+        // Answer ++
+int age= scanner.nextInt();
+int nun2 = scanner.nextInt();
+        scanner.next()
+scanner.nextline();
+
+    }
+}
